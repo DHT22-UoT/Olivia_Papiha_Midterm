4333e0966906d033de0232d6ac15f97c65a30ab8
Papiha Joharapurkar <papihajoharapurkar@Papihas-MacBook-Pro.local>
2022-07-08 23:36:58 -0400

    Adding transformations and comments

--- EDA_Weather.R
+++ EDA_Weather.R
@@ -19,12 +19,12 @@ status(weather_df)
 #q_inf and p_na does not show any infinite-values 
 #q_na is high in max_gust_speed_mph, representing 24.71% of data according to p_na
 #for type of input:
-  #date is in character-form, needs to be converted to date-type
-  #precipitation-inches in character, should be integer
-  #cloud-cover can be factor, only 9 unique values
-  #events can be factor, the "" seen from glimpse-view can be converted to NA, and only 5 unique values
-  #zipcode can turned to factor, only 5 unique values
-  #city can be factor, only 5 unique values
+#date is in character-form, needs to be converted to date-type
+#precipitation-inches in character, should be integer
+#cloud-cover can be factor, only 9 unique values
+#events can be factor, the "" seen from glimpse-view can be converted to NA, and only 5 unique values
+#zipcode can turned to factor, only 5 unique values
+#city can be factor, only 5 unique values
 
 #performing necessary transformations before visualization
 weather_df_1 <- weather_df %>% 
@@ -52,8 +52,8 @@ profiling_num(weather_df)
 #variation coefficient shows that the min_visibility_miles, max_wind_Speed_mph, max_wind_Speed_mph, mean_wind_speed_mph and cloud_cover columns have standard-deviation
 #values that represent high % of mean 
 #for skewness, can see that max_visibility_miles, max_wind_Speed_mph and max_gust_speed_mph are associated with asymmetric distribution
-  #positive-skew shown for mean_visibility_miles, max_visibility_miles, mean_visibility_miles, max_wind_Speed_mph, max_gust_speed_mph
-  #negative skew shown for mean_temperature_f, min_temperature_f, min_visibility_miles columns
+#positive-skew shown for mean_visibility_miles, max_visibility_miles, mean_visibility_miles, max_wind_Speed_mph, max_gust_speed_mph
+#negative skew shown for mean_temperature_f, min_temperature_f, min_visibility_miles columns
 #high kurtosis values for max_visibility_miles, mean_visibility_miles, max_wind_Speed_mph, max_gust_speed_mph columns indicating greater presence of outliers
 #high IQR range for max_temperature_f, mean_temperature_f, min_temperature_f variables as well as zip_code but can be ignored considering future transformation to character-type
 

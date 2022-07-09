dfc87fb683c66174b831b6e996f8ece4a270c0f3
Papiha Joharapurkar <papihajoharapurkar@Papihas-MacBook-Pro.local>
2022-07-08 23:35:40 -0400

    Adding transformations for variables, formatting date column as date

--- EDA_Weather.R
+++ EDA_Weather.R
@@ -1,9 +1,11 @@
 #EDA Weather
 
 #Loading necessary libraries
-library("tidyverse")
-library("funModeling")
-library("Hmisc")
+library(tidyverse)
+library(funModeling)
+library(Hmisc)
+library(dplyr)
+library(lubridate)
 
 #Uploading weather.dataset and assigning to weather_df object
 weather_df <- read.csv("weather.csv")
@@ -24,17 +26,27 @@ status(weather_df)
   #zipcode can turned to factor, only 5 unique values
   #city can be factor, only 5 unique values
 
-#Adding in necessary transformations 
-weather_df$date <- as.POSIXct(weather_df$date, format="%d/%m/%y")
+#performing necessary transformations before visualization
+weather_df_1 <- weather_df %>% 
+  mutate(date = as.POSIXct(date, format="%d/%m/%y")) %>%
+  mutate (precipitation_inches = str_replace(precipitation_inches, pattern="T", replacement="0")) %>% 
+  mutate (precipitation_inches = as.numeric(precipitation_inches)) %>%
+  mutate (cloud_cover = as.factor(cloud_cover)) %>%
+  mutate (events = na_if(x=events, y="")) %>%
+  mutate (events = as.factor(events)) %>%
+  mutate (zip_code = as.factor(zip_code)) %>%
+  mutate (city = as.factor(city))
+
 
 #analyzing categorical data 
-freq(weather_df)
+freq(weather_df_1)
 #informs that precipitation_inches values of T represent 4% of the data, needs to be filtered out 
 #for events, "" value represents 80.71% of data, needs to be filtered out
 
 #analyzing numerical data 
-plot_num(weather_df)
+plot_num(weather_df_1)
 #unbalanced variables: max_visibility_miles, max_wind_speed, max_gust_speed_zip-code 
+
 profiling_num(weather_df)
 #shows high standard deviation for max_temperature_f and mean_temperature_f, as well as zip_code(can be ignored considering will be turned into character-type)
 #variation coefficient shows that the min_visibility_miles, max_wind_Speed_mph, max_wind_Speed_mph, mean_wind_speed_mph and cloud_cover columns have standard-deviation

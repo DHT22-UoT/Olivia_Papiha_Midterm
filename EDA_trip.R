ed74a78ae860bace6566d30761408fc169a3cb40
ozoep <olivia.z.paunkoff@gmail.com>
2022-07-13 00:15:27 -0400

    lots of stuff.

--- EDA_trip.R
+++ EDA_trip.R
@@ -3,6 +3,7 @@
 library(funModeling)
 library(tidyverse)
 library(Hmisc)
+library(plyr)
 
 setwd("~/Users/olivia/Desktop/")
 trip_data <- read_csv("trip.csv")
@@ -23,22 +24,112 @@ basic_eda <- function(trip_data)
 
 basic_eda(trip_data)
 
-# Step 1: First approach to data - glimpse(trip_data)
+# Step 1: First approach to data
+
+glimpse(trip_data)
 
 status(trip_data)
 
-# Step 2: Analyzing categorical variables - freq(trip_data)
+# Noticed there are 70 unique station ids but 74 station names (both start and end stations)
 
-hist(trip_data$)
+# Looking at unique values in start station name to identify duplicates
+unique(sort(trip_data$start_station_name))
+# noticed "Post at Kearney" and "Post at Kearny" as well as "Washington at Kearney" and "Washington at Kearny"
+
+# Looking at unique values in end station name to look for duplicates
+unique(sort(trip_data$end_station_name))
+# "Post at Kearney" and "Post at Kearny" and "Washington at Kearney" and "Washington at Kearney"
+
+unique(sort(trip_data$start_station_id))
+
+# Fixing misspelled station names 
+
+trip_data$start_station_name[trip_data$start_station_name == "Post at Kearny"] <- "Post at Kearney"
+trip_data$start_station_name[trip_data$start_station_name == "Washington at Kearny"] <- "Washington at Kearney"
+
+trip_data$end_station_name[trip_data$end_station_name == "Post at Kearny"] <- "Post at Kearney"
+trip_data$end_station_name[trip_data$end_station_name == "Washington at Kearny"] <- "Washington at Kearney"
+
+start_stat_tab <- data_frame(unique(trip_data$start_station_name))
+start_id_tab <- data_frame(unique(trip_data$start_station_id))
+
+# Found "Broadway at Main" and "Stanford in Redwood City" are both coded under station ID 80
+# Combining them into one station name 
+trip_data$start_station_name[trip_data$start_station_name == "Broadway at Main"] <- "Stanford in Redwood City"
+trip_data$end_station_name[trip_data$end_station_name == "Broadway at Main"] <- "Stanford in Redwood City"
+
+
+# Found "San Jose Government Center" and "Santa Clara County Civic Center" are both coded under station ID 25
+# Combining them into one station name 
+trip_data$start_station_name[trip_data$start_station_name == "San Jose Government Center"] <- "Santa Clara County Civic Center"
+trip_data$end_station_name[trip_data$end_station_name == "San Jose Government Center"] <- "Santa Clara County Civic Center"
+
+status(trip_data)
+
+
+library(lubridate)
+
+# Changing dates from character to a date
+
+#trip_data <- trip_data %>%
+  #mutate(start_date = as.POSIXlt(start_date, format= "%d/%m/%y %H:%M")) %>%
+  #mutate(end_date = as.POSIXlt(end_date, format= "%d/%m/%y %H:%M"))
+
+
+# Step 2: Analyzing categorical variables - 
+
+# Creating a plot of start station frequency 
+freq(trip_data$start_station_name)
+head(freq(trip_data$start_station_name), 10)
+
+# Creating a plot of end station frequency 
+freq(trip_data$end_station_name)
+head(freq(trip_data$end_station_name), 10)
+
+# Plot for subscription type
+freq(trip_data$subscription_type)
+
 
 # Step 3: Analyzing numerical variables - plot_num(trip_data)
 
-# freq(data, path_out = ".") to export to pdf
-
 # Runs for all numerical/integer variables automatically
-data_prof=profiling_num(trip_data)
+profiling_num(trip_data$duration)
+describe(trip_data$duration)
 
-# Step 4: Analyzes numerical and categorical at the same time - describe(trip_data)
+# Find the number of cancelled trips (<2min) and remove from data set 
+
+# Filter rows more than 120s 
+trip_clean <- trip_data %>%
+  filter(duration >= 120)
+
+# removed 2,499 trips
+
+# Removing the outliers from duration
+
+iqr_trip <- IQR(trip_clean$duration)
+# 404
+
+Q1 <- quantile(trip_clean$duration, .25)
+Q3 <- quantile(trip_clean$duration, .75)
+
+up <- 1.5*iqr_trip + Q3 # Upper Range  
+
+low <- 1.5*iqr_trip - Q1 # Lower Range
+
+# trip_clean = 323840 obs
+trip_clean2 <- trip_clean
+
+trip_clean2$duration[trip_clean2$duration > 1352.5] <- 1352.5
+trip_clean2$duration[trip_clean2$duration < 259.5] <- 259.5
+
+# trip_clean2 = 65,195 outliers were assigned as upper and lower limits instead of removing
+
+# Plot histogram of the duration in seconds 
+hist(trip_clean2$duration, main = "Histogram of Trip Duration", xlab = "Trip Duration in Seconds")
+
+# Step 4: Analyzes numerical and categorical at the same time - 
+
+describe(trip_clean2$duration)
 
 # Check min and max values (outliers)
 # Check Distributions (same as before)
--- Just_trip.R
+++ Just_trip.R
@@ -11,6 +11,7 @@ trip_data <- read_csv("trip.csv")
 
 nrow(trip_data$duration < 120) # returning null?
 
+# Filter out rows more than 120s 
 trip_clean <- trip_data %>%
   filter(duration >= 120)
 
@@ -34,7 +35,7 @@ low <- 1.5*iqr_trip - Q1 # Lower Range
 trip_clean2 <- subset(trip_clean, trip_clean$duration > (low) & trip_clean$duration < (up))
 dim(trip_clean2)
 
-# trip_clean2 = 258645 obs, removed 65,195 obs 
+# trip_clean2 = 258,45 obs, removed 65,195 obs 
 
 # Establish the highest volume hours on weekdays. Use to build 'rush hours' into their model (lubridate package).
 # Have to find the hours of weekdays where the trip volume is highest (eg. can try histograms). Just trip.
@@ -42,52 +43,121 @@ dim(trip_clean2)
 # Convert format of start_date and end_date
 
 # So date will be recognized as a date
+# Attempted to mutate data and input format as "%d/%m/%y %H:%M" but dates are returning NA
 as.POSIXct(trip_clean2$start_date)
 as.POSIXct(trip_clean2$end_date)
 
-# puts dates in recognizable formats 
-for_start_date <- mdy_hm(trip_clean2$start_date)
-for_end_date <- mdy_hm(trip_clean2$end_date)
+glimpse(trip_clean2)
+print(status(trip_clean2))
 
-mdy_hm(trip_clean2$start_date)
 
-# establish which dates are weekdays and extract
-
-wday(for_start_date, label = T)
-hour(for_start_date)
+# Check if they are not being recognized correctly 
+trip_clean2$for_start_date
 
 rush_hours <- trip_clean2 %>%
   mutate(start_date_for = mdy_hm(start_date)) %>%
   # Removes Sun (1) and Sat (7) from data
   filter(wday(start_date_for) >= 2 & wday(start_date_for) <= 6) %>%
-  mutate(hours = hour(mdy_hm(start_date))) %>%
-  group_by(hours) %>%
-  tally()
+  mutate(hrs = hour(mdy_hm(start_date))) %>%
+  group_by(hrs)
  
 # Est highest vol hours for each weekday with histogram
-ggplot() + geom_histogram(aes(x=rush_hours$hours, y=rush_hours$n))
+hist(rush_hours$hrs)
 
-hist(rush_hours$hours)
+# And using a table
+table(rush_hours$hrs)
+
+# Determine the 10 most frequent starting stations and ending stations during the ‘rush hours’.
+
+# Most frequent starting stations during rush hours on weekdays
+rush_hours2 <- trip_clean2 %>%
+  mutate(start_date_for = mdy_hm(start_date)) %>%
+  # Removes Sun (1) and Sat (7) from data
+  filter(wday(start_date_for) >= 2 & wday(start_date_for) <= 6) %>%
+  mutate(hrs = hour(mdy_hm(start_date))) %>%
+  group_by(hrs) %>%
+  group_by(start_station_name)
+
+table(rush_hours2$start_station_name)
+
+# Most frequent ending stations during rush hours on weekdays
+rush_hours3 <- trip_clean2 %>%
+  mutate(end_date_for = mdy_hm(end_date)) %>%
+  # Removes Sun (1) and Sat (7) from data
+  filter(wday(end_date_for) >= 2 & wday(end_date_for) <= 6) %>%
+  mutate(hrs = hour(mdy_hm(end_date))) %>%
+  group_by(hrs) %>%
+  group_by(end_station_name)
+
+table(rush_hours3$end_station_name)
+head((rush_hours3$end_station_name),10)
+
+# Determine the 10 most frequent starting stations and ending stations during weekends. 
+
+# Most frequent starting stations on weekends
+rush_hours_wkend <- trip_clean2 %>%
+  mutate(start_date_for = mdy_hm(start_date)) %>%
+  # Removes everyday except for Sun (1) and Sat (7) from data
+  filter(wday(start_date_for) == 1 | wday(start_date_for) == 7) %>%
+  mutate(hrs = hour(mdy_hm(start_date))) %>%
+  # Group by hours
+  group_by(hrs) %>%
+  # Group by start stations
+  group_by(start_station_name)
+
+table(rush_hours_wkend$start_station_name)
+
+# Most frequent ending stations on weekends
+rush_hours_wkend2 <- trip_clean2 %>%
+  mutate(end_date_for = mdy_hm(end_date)) %>%
+  # Removes everyday except for Sun (1) and Sat (7) from data
+  filter(wday(end_date_for) == 1 | wday(end_date_for) == 7) %>%
+  mutate(hrs = hour(mdy_hm(end_date))) %>%
+  group_by(hrs) %>%
+  group_by(end_station_name)
+
+table(rush_hours_wkend2$end_station_name)
 
 
 # Calculate the average utilization of bikes for each month. Total time used/total time in month.
 # New df 
 # Group by month (mutate)
-# Sum duration per month
+# Sum duration per bike_id per month
 # Add column for number of seconds in each month
 # Add month summarise sum_duration/ no of sec per month
 
 bike_utl <- trip_clean2 %>%
   mutate(start_date_for2 = mdy_hm(start_date)) %>%
-  group_by(month = month(start_date_for2))
+  group_by(bike_id) %>%
+  group_by(mon = month(start_date_for2))
 
-bike_utl2 <- aggregate(bike_utl$duration, by=list(month = bike_utl$month), FUN = sum)
+
+bike_utl2 <- aggregate(bike_utl$duration, by=list(bike_id=bike_utl$bike_id ,mon=bike_utl$mon),FUN = sum)
 
 bike_utl2 %>%
-  # Add column with seconds per month 
-  mutate(sec_per_month = c(2678400, 2419200, 2678400, 2592000,2678400,2592000,2678400,
-                           2678400,2592000, 2678400,2592000,2678400)) %>%
+  # add column for number of days per month 
+  mutate(days_in_month = ifelse(1, 31), 
+                                ifelse(2, 28),
+                                       ifelse(3, 31),
+                                              ifelse(4, 30),
+                                                     ifelse(5, 31),
+                                                            ifelse(6, 30),
+                                                                   ifelse(7, 31),
+                                                                          ifelse(8, 31),
+                                                                                 ifelse(9, 30),
+                                                                                        ifelse(10,31),
+                                                                                               ifelse(11,30),
+                                                                                                      ifelse(12, 31)) %>%
+# Add column with calculation seconds per month (86400 sec / day)
+  mutate(sec_per_month = days_in_month*86400) %>%
   mutate(avg_utl_per_mon = x/sec_per_month)
 
+# Summed the duration in seconds of trip starting in each month 
+#bike_utl2 <- aggregate(bike_utl$duration, by=list(mon = bike_utl$month), FUN = sum)
 
+#bike_utl2 %>%
+# add column for number of days per month 
+ #mutate(no_days_per_month = c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)) %>%
+# Add column with calculation seconds per month (86400 sec / day)
+#mutate(sec_per_month = no_days_per_month*86400)
 
